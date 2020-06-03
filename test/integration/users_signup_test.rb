2b510fc7650643846324f8029c509f676f198d15
ywenli <kuota0204@gmail.com>
2020-06-03 05:26:32 +0000

    Add account activation to user registration

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -27,10 +27,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       password_confirmation: "password"} }
     end
     follow_redirect!
-    assert_template 'users/show'
+    #assert_template 'users/show'
     assert_not flash.empty?
     # テストユーザーがログインしている
-    assert is_logged_in?
+    #assert is_logged_in?
     
   end
 end
