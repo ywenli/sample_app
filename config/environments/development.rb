f52ea9df0b75617163963000dd350e376da2a43c
ywenli <kuota0204@gmail.com>
2020-06-03 04:34:24 +0000

    Make it possible to show a preview of the email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,9 +27,10 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = '51b442b72a43404aab936c0c57912637.vfs.cloud9.ap-northeast-1.amazonaws.com'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -3,7 +3,11 @@ class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/account_activation
   def account_activation
-    UserMailer.account_activation
+    user = User.first
+    # userのactivation_tokenに有効化トークンを代入
+    user.activation_token = User.new_token
+    # UserMailerクラスのaccount_activationメソッドを呼び出し（引数にuserを渡す
+    UserMailer.account_activation(user)
   end
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
