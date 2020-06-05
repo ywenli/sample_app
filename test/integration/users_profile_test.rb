d5d0530586e65edf773a0e5f28d84e3c081fb585
ywenli <kuota0204@gmail.com>
2020-06-05 07:28:27 +0000

    Update the integration test

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class UsersProfileTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  # ApplicationHelperの読み込み = full_titleヘルパーが使える
+  include ApplicationHelper
+  
+  def setup
+    @user = users(:michael)
+  end
+  
+  test "profile display" do
+    get user_path(@user)
+    assert_template 'users/show'
+    assert_select 'title', full_title(@user.name)
+    assert_select 'h1', text: @user.name
+    # h1のタグに含まれるimg.gravatar
+    assert_select 'h1>img.gravatar'
+    # @userのマイクロポストのcountを文字列にしたもの
+    assert_match @user.microposts.count.to_s, response.body
+    # class = "pagination"を持つdivタグが1こ
+    assert_select 'div.pagination', count: 1
+    # @user.micropostsのページネーションの1ページ目の配列を1個ずつ取り出してmicropostに代入
+    @user.microposts.paginate(page: 1).each do |micropost|
+      assert_match micropost.content, response.body
+    end
+  end
 end
