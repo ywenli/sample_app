5d86e4c9b963ee8c71078e9323ea30b9f3aa7b77
ywenli <kuota0204@gmail.com>
2020-06-05 03:03:54 +0000

    Update to show microposts in descending order

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,7 @@ class Micropost < ApplicationRecord
   # 自動生成
   # MicropostとそのUserは belongs_to(1対1)の関係
   belongs_to :user
+  default_scope -> { order(created_at: :desc) }
   # user_idが存在する
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
