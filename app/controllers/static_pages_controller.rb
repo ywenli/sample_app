70085bf3e4326912818078e3d8125f62d4aff8ff
ywenli <kuota0204@gmail.com>
2020-06-05 09:23:20 +0000

    Add a micropost form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
+  
   def home
+    # @micropostに代入 現在のユーザーに紐づいたMicropostオブジェクトを返す
+    # ユーザーがログインしていれば
+    @micropost = current_user.microposts.build if logged_in?
   end
 
   def help
