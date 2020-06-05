b83137d14a5ff9a2d8cfb577073bfa50d4c05771
ywenli <kuota0204@gmail.com>
2020-06-05 15:31:23 +0000

    Add feed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,13 @@
 class StaticPagesController < ApplicationController
   
   def home
-    # @micropostに代入 現在のユーザーに紐づいたMicropostオブジェクトを返す
-    # ユーザーがログインしていれば
-    @micropost = current_user.microposts.build if logged_in?
+    if logged_in?
+      # @micropostに代入 現在のユーザーに紐づいたMicropostオブジェクトを返す
+      # ユーザーがログインしていれば
+      @micropost = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
+    
   end
 
   def help
