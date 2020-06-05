b7ff830bb96d2fea9c62b41d247f019d5669e1cd
ywenli <kuota0204@gmail.com>
2020-06-05 01:27:44 +0000

    Add a tests for validation

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,7 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  # user_idが存在する
+  validates :user_id, presence: true
+  validates :content, presence: true, length: { maximum: 140 }
+  
 end
