335e0b0cc1362816c7b81f063c16a511ae56dcd1
ywenli <kuota0204@gmail.com>
2020-06-06 07:48:28 +0000

    Add validation to post an image

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -8,5 +8,17 @@ class Micropost < ApplicationRecord
   # user_idが存在する
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
-
+  # シンボル名:picturesize に対応したメソッドを呼び出す
+  validate :picture_size
+  
+  private
+  
+  # アップロードされた画像のサイズをバリデーションする
+  def picture_size
+    # 5MBを超えた場合は
+    if picture.size > 5.megabytes
+      # カスタムのエラーメッセージをerrorsコレクションに追加
+      errors.add(:picture, "should be less than 5MB")
+    end
+  end
 end
