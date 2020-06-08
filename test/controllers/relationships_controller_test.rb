66a53715f767b94511d143a3304312c98a2ed49f
ywenli <kuota0204@gmail.com>
2020-06-08 07:22:31 +0000

    Add test

--- test/controllers/relationships_controller_test.rb
+++ test/controllers/relationships_controller_test.rb
@@ -11,9 +11,8 @@ class RelationshipsControllerTest < ActionDispatch::IntegrationTest
   
   test "destroy should require logged-in user" do
     assert_no_difference 'Relationship.count' do
-      delete relationships_path(relationships(:one))
+      delete relationship_path(relationships(:one))
     end
     assert_redirected_to login_url
   end
-
 end
